824d4a920c5b30866c12ab022ed8894ad5c0af3f
okorkov <alex.okorkov@gmail.com>
2020-11-10 17:46:18 -0800

    commit tree 27f3265b7533d3558629db10a733059489733087

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+    result = array.clone
+    result.sort
+end
 
+def sort_array_desc(array)
+    result = array.clone
+    result.sort.reverse
+end
+
+def sort_array_char_count(strings)
+    result = strings.clone
+    strings.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    result = array.clone
+    array[1] = result[2]
+    array[2] = result[1]
+    array
+end
+
+def swap_elements_from_to(array, from, to)
+    result = array.clone
+    array[to] = result[from]
+    array[from] = result[to]
+    array
+end
+
+def reverse_array(array)
+    result = array.clone
+    result.reverse
+end
+
+def kesha_maker(array)
+    result = Array.new
+    array.each do |string|
+        string[2] = '$'
+        result << string
+    end
+    result
+end
+
+def find_a(array)
+    array.select {|string| string.start_with?('a')}
+end
+
+def sum_array(array)
+    array.inject {|sum, number| sum + number}
+end
+
+def add_s(array)
+    result = Array.new
+    array.each_with_index.collect do |element, index| 
+      if index != 1
+        result << "#{element}s"
+      else
+        result << element
+      end
+    end
+    result
+end
